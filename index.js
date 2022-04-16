149d7d639849dddc42a14e255bdd88a071231b3f
Raya <mail.raya15@gmail.com>
2022-04-16 21:55:24 +0700

    add index html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Responsive Web Developer</h1>");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 routes.forEach((route) => {
